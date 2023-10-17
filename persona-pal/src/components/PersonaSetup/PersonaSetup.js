a4fed33d70b49dd67cc15405f255724d600c0472
Ivan Saldarriaga <saldarriagaij@gmail.com>
2023-10-17 16:43:24 -0400

    Edit Layout & Drawer Widget
    Layout now in grid format, drawer is onclick edit of variable size (shows info when clicked)

--- persona-pal/src/components/PersonaSetup/PersonaSetup.js
+++ persona-pal/src/components/PersonaSetup/PersonaSetup.js
@@ -2,6 +2,7 @@ import './PersonaSetup.css'
 import Navbar from '../Navbar/Navbar'
 import Multiselect from "multiselect-react-dropdown";
 import SelectTraits from '../../assets/Select Traits.svg';
+import arrowIcon from "../../assets/arrow.svg"
 import { useState } from 'react';
 const ProfileSetup = () => {
     const [selection1, setSelection1] = useState(["op1","op2","op3","op4"]);
@@ -9,58 +10,77 @@ const ProfileSetup = () => {
     const [selection3, setSelection3] = useState(["op1","op2","op3","op4"]);
     const [selection4, setSelection4] = useState(["op1","op2","op3","op4"]);
     const [selection5, setSelection5] = useState(["op1","op2","op3","op4"]);
+    const [drawerVisibility, setDrawerVisibility] = useState(false);
+    const toggleVisibility = () => {
+        setDrawerVisibility(!drawerVisibility);
+    }
     return (  
-        <div className = "page-container">
+        <>
             <Navbar/>
-            <div className="independentVar">
-                <img src={SelectTraits} alt='header'/>
-                <div className='dropdown'>
-                    <Multiselect
-                    options={selection1}
-                    isObject = {false}
-                    singleSelect = {true}
-                    onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                    onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
-                    placeholder="selection1" // Property name to display in the dropdown options
-                    />
-                    <Multiselect
-                    options={selection2}
-                    isObject = {false}
-                    singleSelect = {true}
-                    onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                    onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
-                    placeholder="selection2" // Property name to display in the dropdown options
-                    />
-                    <Multiselect
-                    options={selection3}
-                    isObject = {false}
-                    singleSelect = {true}
-                    onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                    onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
-                    placeholder="selection3" // Property name to display in the dropdown options
-                    />
-                    <Multiselect
-                    options={selection4}
-                    isObject = {false}
-                    singleSelect = {true}
-                    onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                    onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
-                    placeholder="selection4" // Property name to display in the dropdown options
-                    />
-                    <Multiselect
-                    options={selection5}
-                    isObject = {false}
-                    singleSelect = {true}
-                    onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                    onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
-                    placeholder="selection5" // Property name to display in the dropdown options
-                    />
+            <div className = "page-container">
+                <div className="independentVar">
+                    <img src={SelectTraits} alt='header'/>
+                    <div className='dropdown'>
+                        <Multiselect
+                        options={selection1}
+                        isObject = {false}
+                        singleSelect = {true}
+                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
+                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        placeholder="selection1" // Property name to display in the dropdown options
+                        />
+                        <Multiselect
+                        options={selection2}
+                        isObject = {false}
+                        singleSelect = {true}
+                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
+                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        placeholder="selection2" // Property name to display in the dropdown options
+                        />
+                        <Multiselect
+                        options={selection3}
+                        isObject = {false}
+                        singleSelect = {true}
+                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
+                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        placeholder="selection3" // Property name to display in the dropdown options
+                        />
+                        <Multiselect
+                        options={selection4}
+                        isObject = {false}
+                        singleSelect = {true}
+                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
+                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        placeholder="selection4" // Property name to display in the dropdown options
+                        />
+                        <Multiselect
+                        options={selection5}
+                        isObject = {false}
+                        singleSelect = {true}
+                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
+                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        placeholder="selection5" // Property name to display in the dropdown options
+                        />
+                    </div>
+                    <div className='buttonArea'>
+                        <button className='submitButton'>Submit!</button>
+                    </div>
                 </div>
-                <div className='buttonArea'>
-                    <button className='submitButton'>Submit!</button>
+                <div className='imageArea'>
+                    img area
+                </div>
+                <div className='savedImages'>
+                    <div className='arrowArea'>
+                        <img src={arrowIcon} alt='arrowIcon' onClick={toggleVisibility}/>
+                    </div>
+                    {drawerVisibility && (
+                        <div className='savedDetails'>
+                            NO SAVED IMAGES
+                        </div>
+                    )}
                 </div>
             </div>
-        </div>
+        </>
     );
 }
  
