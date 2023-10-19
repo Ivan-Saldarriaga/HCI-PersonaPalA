69d7ebd08884e6507fbd2dddf1b42b207e490944
Ivan Saldarriaga <saldarriagaij@gmail.com>
2023-10-19 10:47:01 -0400

    Setting up array to send
    submit button console.log issues

--- persona-pal/src/components/PersonaSetup/PersonaSetup.js
+++ persona-pal/src/components/PersonaSetup/PersonaSetup.js
@@ -3,7 +3,7 @@ import Navbar from '../Navbar/Navbar'
 import Multiselect from "multiselect-react-dropdown";
 import SelectTraits from '../../assets/Select Traits.svg';
 import arrowIcon from "../../assets/arrow.svg";
-import baseImg from "../../assets/download.jpg";
+import baseImg from "../../assets/512.png";
 import { useState } from 'react';
 const ProfileSetup = () => {
     const [selection1, setSelection1] = useState(["op1","op2","op3","op4"]);
@@ -11,6 +11,34 @@ const ProfileSetup = () => {
     const [selection3, setSelection3] = useState(["op1","op2","op3","op4"]);
     const [selection4, setSelection4] = useState(["op1","op2","op3","op4"]);
     const [selection5, setSelection5] = useState(["op1","op2","op3","op4"]);
+    const [selectedOptions, setSelectedOptions] = useState(["", "", "", "", ""]);
+    const handleDropdownSelect = (index, selectedOption) => {
+        const newSelectedOptions = [...selectedOptions];
+        newSelectedOptions[index - 1] = selectedOption;
+        setSelectedOptions(newSelectedOptions);
+    };
+
+    const handleDropdownRemove = (index) => {
+        const newSelectedOptions = [...selectedOptions];
+        newSelectedOptions[index - 1] = "";
+        setSelectedOptions(newSelectedOptions);
+    };
+    const handleSubmit = () => {
+        // Check if any of the selected strings are empty
+        if (selectedOptions.some((option) => option === "")) {
+            // If any of the strings are empty, prevent the submission
+            console.log("Please select an option from all dropdowns.");
+            return;
+        }
+
+        // If all strings are selected, proceed with the submission
+        console.log(selectedOptions);
+        // You can use selectedOptions as needed for the submission.
+    };
+
+
+
+
     const [drawerVisibility, setDrawerVisibility] = useState(false);
     const [imageFlipped, flipImage] = useState(false);
     const toggleVisibility = () => {
@@ -28,50 +56,52 @@ const ProfileSetup = () => {
                         options={selection1}
                         isObject = {false}
                         singleSelect = {true}
-                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        onSelect={(selectedOption) => handleDropdownSelect(1, selectedOption)}
+                        onRemove={() => handleDropdownRemove(1)}
                         placeholder="selection1" // Property name to display in the dropdown options
                         />
                         <Multiselect
                         options={selection2}
                         isObject = {false}
                         singleSelect = {true}
-                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        onSelect={(selectedOption) => handleDropdownSelect(2, selectedOption)}
+                        onRemove={() => handleDropdownRemove(2)}
                         placeholder="selection2" // Property name to display in the dropdown options
                         />
                         <Multiselect
                         options={selection3}
                         isObject = {false}
                         singleSelect = {true}
-                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        onSelect={(selectedOption) => handleDropdownSelect(3, selectedOption)}
+                        onRemove={() => handleDropdownRemove(3)}
                         placeholder="selection3" // Property name to display in the dropdown options
                         />
                         <Multiselect
                         options={selection4}
                         isObject = {false}
                         singleSelect = {true}
-                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        onSelect={(selectedOption) => handleDropdownSelect(4, selectedOption)}
+                        onRemove={() => handleDropdownRemove(4)}
                         placeholder="selection4" // Property name to display in the dropdown options
                         />
                         <Multiselect
                         options={selection5}
                         isObject = {false}
                         singleSelect = {true}
-                        onSelect={(event) => {console.log(event)}} // Function will trigger on select event
-                        onRemove={(event) => {console.log(event)}} // Function will trigger on remove event
+                        onSelect={(selectedOption) => handleDropdownSelect(5, selectedOption)}
+                        onRemove={() => handleDropdownRemove(5)}
                         placeholder="selection5" // Property name to display in the dropdown options
                         />
                     </div>
                     <div className='buttonArea'>
-                        <button className='submitButton'>Submit!</button>
+                        <button className='submitButton' onClick={handleSubmit}>Submit!</button>
                     </div>
                 </div>
                 <div className='imageArea'>
                     <div className='imageHolder'>
-                        <img src={baseImg} alt='baseimg'/>
+                        <div className='imageContainer'>
+                            {/* <img src={baseImg} alt='baseimg'/> */}
+                        </div>
                     </div>
                 </div>
                 <div className='savedImages'>
