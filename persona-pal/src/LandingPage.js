2fb187a9a2a2a3e920a1bc2714eefb49007004bd
Wavid Bowman <wavid.bowman@ufl.edu>
2023-10-23 20:01:17 -0400

    much better

--- persona-pal/src/LandingPage.js
+++ persona-pal/src/LandingPage.js
@@ -9,6 +9,7 @@ import subtitle from './assets/LandingPage/subtitle.svg';
 import title from './assets/LandingPage/title.svg';
 import team from './assets/LandingPage/team.svg';
 import welcome from './assets/LandingPage/welcome.svg';
+import fullTitle from './assets/LandingPage/title0.svg';
 
 import './App.css';
 import { GoogleLogin } from '@react-oauth/google';
@@ -36,25 +37,16 @@ function LandingPage() {
     return (
         <GoogleOAuthProvider clientId="176425871896-2jfjino16tho0kvlrjfqmqc6qs32nfqa.apps.googleusercontent.com">
         <div className="LandingPage">
-            <header className="App-header">
-                <background-image src={background} className="background"  />
-                <img src={welcome} className="welcome" />
-                <img src={title} className="title" />
-                <img src={subtitle} className="subtitle" />
-                <img src={loginButton} className="loginButton" />
-                <GoogleLogin
-                    clientId="176425871896-2jfjino16tho0kvlrjfqmqc6qs32nfqa.apps.googleusercontent.com"
-                    buttonText="Login with Google"
-                    onSuccess={handleGoogleLoginSuccess}
-                    onFailure={handleGoogleLoginFailure}
-                    cookiePolicy={'single_host_origin'}
-                />
-                <img src={team} className="team" />
+        
+            <img src={fullTitle} className="fullTitle" />
+            <img src={loginButton} className="loginButton" />
+            
+            <img src={team} className="team" />
 
 
 
                 
-            </header>
+            
         </div>
         </GoogleOAuthProvider>
     );
